03a3326df94d5eeb483ea8d6603505d803715e4a
Vito Gamberini <vito@gamberini.email>
2022-12-21 11:37:10 -0500

    bump

--- flash.sh
+++ flash.sh
@@ -1,10 +1,9 @@
 port=""
+
 while [ -z $port ]
 do
   port=`find /dev -name 'ttyACM*'`
 done
 
-echo "Running: avrdude -p m32u4 -c avr109 -U flash:w:build/release/flash:e $port"
-avrdude -p m32u4 -c avr109 -U flash:w:build/release/flash:e $port
-
-cat $port
+echo "Found $port"
+avrdude -p m32u4 -c avr109 -U flash:w:build/Release/flash:e -P $port
